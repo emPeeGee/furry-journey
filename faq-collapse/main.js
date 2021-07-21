afc8dc138f01bc6f605b092cd6a9234ce11957de
Mihail Popovici <mihail.popovici@energymarketprice.com>
2021-07-21 13:36:04 +0300

    feat: Make faq-collapse

---
+++ faq-collapse/main.js
@@ -0,0 +1,7 @@
+const faqToggles = document.querySelectorAll('.faq-toggle');
+
+faqToggles.forEach((faqToggle) => {
+  faqToggle.addEventListener('click', () => {
+    faqToggle.parentNode.classList.toggle('active');
+  });
+});
