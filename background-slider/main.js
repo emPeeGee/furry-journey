db5a1709d876a416e73f99440cab24576e4680d1
Mihail Popovici <eempeegee@gmail.com>
2021-07-24 09:26:40 +0300

    feat: Make background-slider mini-project

---
+++ background-slider/main.js
@@ -0,0 +1,39 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftButton = document.querySelector('#left');
+const rightButton = document.querySelector('#right');
+
+let activeSlide = 0;
+
+setBackgroundToBody();
+
+leftButton.addEventListener('click', () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  setBackgroundToBody();
+  setActiveSlide();
+});
+
+rightButton.addEventListener('click', () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBackgroundToBody();
+  setActiveSlide();
+});
+
+function setBackgroundToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove('active'));
+  slides[activeSlide].classList.add('active');
+}
